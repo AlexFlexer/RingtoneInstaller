41a00efcf1d01acd9368bde9440125b9e0755bcd
Александр Дяченко <a.dyachenko@tirscript.com>
2022-01-02 23:40:28 +0200

    TracksFragment.kt: temp commit

--- app/src/main/java/com/axelpetprojects/ringtoner/TracksFragment.kt
+++ app/src/main/java/com/axelpetprojects/ringtoner/TracksFragment.kt
@@ -1,9 +1,19 @@
 package com.axelpetprojects.ringtoner
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.axelpetprojects.ringtoner.databinding.FragmentTracksBinding
 
 class TracksFragment : Fragment(R.layout.fragment_tracks) {
     private val mBinding: FragmentTracksBinding by viewBinding()
+    private val mViewModel: TracksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mViewModel.tracksData.observe(viewLifecycleOwner) {
+
+        }
+    }
 }
