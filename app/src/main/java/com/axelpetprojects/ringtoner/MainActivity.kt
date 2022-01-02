c58beeccc02e44155beb77f77c255cd9f7892cba
Александр Дяченко <a.dyachenko@tirscript.com>
2022-01-02 22:20:28 +0200

    MainActivity.kt: simplified code

--- app/src/main/java/com/axelpetprojects/ringtoner/MainActivity.kt
+++ app/src/main/java/com/axelpetprojects/ringtoner/MainActivity.kt
@@ -1,18 +1,6 @@
 package com.axelpetprojects.ringtoner
 
-import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import by.kirich1409.viewbindingdelegate.viewBinding
-import com.axelpetprojects.ringtoner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    private val mBinding: ActivityMainBinding by viewBinding()
-    private val mViewModel: TracksViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 }
