c7c89da8baa8c979f11ead23e29b0eba5541d918
Александр Дяченко <a.dyachenko@tirscript.com>
2022-01-03 23:37:59 +0200

    TracksFragment.kt: permission granting process reworked

--- app/src/main/java/com/axelpetprojects/ringtoner/TracksFragment.kt
+++ app/src/main/java/com/axelpetprojects/ringtoner/TracksFragment.kt
@@ -1,7 +1,10 @@
 package com.axelpetprojects.ringtoner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -13,7 +16,13 @@ import com.axelpetprojects.ringtoner.adapters.TextAdapter
 import com.axelpetprojects.ringtoner.adapters.TracksAdapter
 import com.axelpetprojects.ringtoner.databinding.FragmentTracksBinding
 
+@Suppress("deprecation")
 class TracksFragment : Fragment(R.layout.fragment_tracks), SwipeRefreshLayout.OnRefreshListener {
+
+    companion object {
+        private const val PERMISSIONS_CODE = 1
+    }
+
     private val mBinding: FragmentTracksBinding by viewBinding()
     private val mViewModel: TracksViewModel by viewModels()
 
@@ -25,12 +34,32 @@ class TracksFragment : Fragment(R.layout.fragment_tracks), SwipeRefreshLayout.On
 
     override fun onRefresh() = mViewModel.fetchAudios(true)
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_CODE) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                mViewModel.fetchAudios(false)
+            } else {
+                Toast.makeText(requireContext(), R.string.go_to_settings, Toast.LENGTH_LONG).show()
+                requireContext().openAppSettings()
+            }
+        }
+    }
+
     private fun handleTracks(event: TrackEvent) {
         mBinding.refresherTracks.isRefreshing = false
         val adapter = when (event) {
             is TrackEvent.Error -> TextAdapter(event.msg)
             is TrackEvent.Loading -> LoadingAdapter()
-            is TrackEvent.NoPermission -> NoPermissionAdapter { mViewModel.fetchAudios(false) }
+            is TrackEvent.NoPermission -> NoPermissionAdapter {
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_CODE
+                )
+            }
             is TrackEvent.Result -> TracksAdapter(event.tracks) {
                 mViewModel.selectTrack(it)
                 findNavController().navigate(R.id.tracks_dialog)
--- app/src/main/java/com/axelpetprojects/ringtoner/Utils.kt
+++ app/src/main/java/com/axelpetprojects/ringtoner/Utils.kt
@@ -1,8 +1,54 @@
 package com.axelpetprojects.ringtoner
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 
 val ViewGroup.layoutInflater: LayoutInflater
     get() = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+fun Context.openAppSettings(shouldOpenAsNewTask: Boolean = true) =
+    startActivityOrFallback(prepareIntentForDetailSettings(packageName, shouldOpenAsNewTask)) {
+        notifyAboutNoSettings()
+    }
+
+fun Context.startActivityOrFallback(intent: Intent, fallback: () -> Unit = { notifyAboutNoApp() }) =
+    tryOrGiveUp({ startActivity(intent) }, { fallback() })
+
+/**
+ * Tries to perform some [actionToTry], and if it throws an exception, returns [defaultVal].
+ */
+inline fun <T> tryOrGiveUp(defaultVal: T, actionToTry: () -> T): T =
+    try {
+        actionToTry()
+    } catch (t: Exception) {
+        defaultVal
+    }
+
+private fun prepareIntentForDetailSettings(
+    packageName: String,
+    shouldOpenAsNewTask: Boolean
+): Intent {
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+    if (shouldOpenAsNewTask) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    val uri = Uri.fromParts("package", packageName, null)
+    intent.data = uri
+    return intent
+}
+
+private fun Context.notifyAboutNoApp() = showNoAppDialog(R.string.no_app_for_action)
+
+private fun Context.notifyAboutNoSettings() = showNoAppDialog(R.string.no_app_for_settings)
+
+private fun Context.showNoAppDialog(@StringRes message: Int) {
+    createAlertDialogWithMessage(
+        this,
+        R.string.error,
+        message,
+        R.string.ok
+    ).show()
+}
