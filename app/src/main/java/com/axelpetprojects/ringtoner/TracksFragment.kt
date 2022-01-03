4e8f895be396622882c20869d261db5bcd18d555
Александр Дяченко <a.dyachenko@tirscript.com>
2022-01-03 23:15:09 +0200

    TracksFragment.kt: completed

--- app/src/main/java/com/axelpetprojects/ringtoner/TracksFragment.kt
+++ app/src/main/java/com/axelpetprojects/ringtoner/TracksFragment.kt
@@ -4,16 +4,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.axelpetprojects.ringtoner.adapters.LoadingAdapter
+import com.axelpetprojects.ringtoner.adapters.NoPermissionAdapter
+import com.axelpetprojects.ringtoner.adapters.TextAdapter
+import com.axelpetprojects.ringtoner.adapters.TracksAdapter
 import com.axelpetprojects.ringtoner.databinding.FragmentTracksBinding
 
-class TracksFragment : Fragment(R.layout.fragment_tracks) {
+class TracksFragment : Fragment(R.layout.fragment_tracks), SwipeRefreshLayout.OnRefreshListener {
     private val mBinding: FragmentTracksBinding by viewBinding()
     private val mViewModel: TracksViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        mViewModel.tracksData.observe(viewLifecycleOwner) {
+        mBinding.refresherTracks.setOnRefreshListener(this)
+        mViewModel.tracksData.observe(viewLifecycleOwner) { handleTracks(it) }
+        mViewModel.fetchAudios(false)
+    }
 
+    override fun onRefresh() = mViewModel.fetchAudios(true)
+
+    private fun handleTracks(event: TrackEvent) {
+        mBinding.refresherTracks.isRefreshing = false
+        val adapter = when (event) {
+            is TrackEvent.Error -> TextAdapter(event.msg)
+            is TrackEvent.Loading -> LoadingAdapter()
+            is TrackEvent.NoPermission -> NoPermissionAdapter { mViewModel.fetchAudios(false) }
+            is TrackEvent.Result -> TracksAdapter(event.tracks) {
+                findNavController().navigate(R.id.tracks_dialog)
+            }
         }
+        mBinding.recyclerTracks.adapter = adapter
     }
 }
